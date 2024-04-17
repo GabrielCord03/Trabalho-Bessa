5feb479b0cd050b6e1c22c7ab9c78111f10a06c7
GabrielCord03 <141854199+GabrielCord03@users.noreply.github.com>
2024-04-16 21:31:44 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Contato
+{
+    public string Nome { get; set; }
+    public string Telefone { get; set; }
+    public string Email { get; set; }
+}
+
+public class Evento
+{
+    public string Id 
+    public string Titulo { get; set; }
+    public DateTime Data { get; set; }
+    public DateTime Horaini { get; set; }
+    public DateTime HoraFim { get; set; }
+    public string Descricao { get; set; }
+    public int QuantidadePessoas { get; set; }
+    public string PublicoAlvo { get; set; }
+    public Contato Responsavel { get; set; }
+
+    public Evento()
+    {
+        
+        Id = GerarId();
+    }
+
+    private string GerarId()
+    {
+        
+        Random random = new Random();
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        return new string(Enumerable.Repeat(chars, 6).Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+}
